6878e32736472ff4acd7af1880212d4cbc0db178
Sambo64 <sambo.barker64@gmail.com>
2016-05-16 23:38:46 +0800

    Completed scraper and unit creating

--- scrapertests/scrapertest.py
+++ scrapertests/scrapertest.py
@@ -1,7 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import getpass
+import re
+import time
 
+days = {
+    'Monday' : 0,
+    'Tuesday' : 1,
+    'Wednesday' : 2,
+    'Thursday' : 3,
+    'Friday' : 4 }
+    
 username = input("Input StudentID: ")
 password = getpass.getpass()
 
@@ -29,12 +38,20 @@ with requests.Session() as s:
         print (units[i] + "\n")
         for classtype in unit.select('.cssTtableNavActvTop'):
             type = classtype.select('.cssTtableSspNavActvNm')
-            time = classtype.select('.cssTtableNavMainWhen .cssTtableNavMainContent')
+            when = classtype.select('.cssTtableNavMainWhen .cssTtableNavMainContent')
             where = classtype.select('.cssTtableNavMainWhere .cssTtableNavMainContent')
-            
+            daytime = []
             print(''.join(type[0].findAll(text=True)).strip())
-            print(''.join(time[0].findAll(text=True)).strip())
-            print(''.join(where[0].findAll(text=True)).strip() + "\n")
+            print(''.join(where[0].findAll(text=True)).strip())
+            swhen = ''.join(when[0].findAll(text=True)).strip()
+            daytime = swhen.replace("-", " ").split()
+            day = daytime[0]
+            start = [daytime[1], daytime[2]]
+            end = [daytime[3], daytime[4]]
+            
+            print(day)
+            print(start)
+            print(end)
             
         print("\n\n")
         i = i + 1
--- timetablescraper.py
+++ timetablescraper.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import getpass
+import time
+import 
 
 class Unit:
     def __init__(self, name, classEvents):
@@ -8,35 +10,73 @@ class Unit:
         self.classEvents = classEvents
     
 class ClassEvent:
-    def __init__(self, type, time, place)
+    def __init__(self, type, start, end, day, place):
         self.type = type
-        self.time = time
+        self.start = start
+        self.end = end
+        self.day = day
         self.place = place
 
+def formatWhen(inString):
+    return inString.replace("-", " ").split()
+
+    
+def makeClassEvent(type, when, where):
+    day = when[0]
+    start = [when[1], when[2]]
+    end = [when[3], when[4]]
+    
+    return ClassEvent(type, start, end, day, where)
     
 class Scraper:
     def __init__(self, username, password):
-        self.units[] = units
+        self.units = []
         
         payload = {
         'UserName' : username,
         'Password' : password,
         'submit' : 'Login' 
         }
-        
-        login_url = 'https://oasis.curtin.edu.au'
+        login_url = 'https://oasis.curtin.edu.au/Auth/LogOn'
         timetable_url = 'https://estudent.curtin.edu.au/eStudent/SM/StudentTtable10.aspx?r=%23CU.ESTU.STUDENT&f=%23CU.EST.TIMETBL.WEB'
         
-        with requests.sessions as session:
-            p = session.post(url, data = payload)
+        with requests.Session() as session:
+            p = session.post(login_url, data = payload)
             r = session.get(timetable_url)
             data = r.text
-            soup = BeautifulSoup(data)
+            soup = BeautifulSoup(data, "html.parser")
+        
+        unitName = []
+        print("Mark 2")
+        for unit in soup.select('.cssTtableSspNavMasterSpkInfo2'):
+            unitName.append(''.join(unit.findAll(text=True)))
+
+        i = 0
+        for unit in soup.select('.cssTtableSspNavDetailsContainerPanel'):
+            events = []
+            for classevent in unit.select('.cssTtableNavActvTop'):
+                raw_type = classevent.select('.cssTtableSspNavActvNm')
+                raw_time = classevent.select('.cssTtableNavMainWhen .cssTtableNavMainContent')
+                raw_where = classevent.select('.cssTtableNavMainWhere .cssTtableNavMainContent')
+
+                type = ''.join(raw_type[0].findAll(text=True)).strip()
+                time = ''.join(raw_time[0].findAll(text=True)).strip()
+                where = ''.join(raw_where[0].findAll(text=True)).strip()
+                
+                when = formatWhen(time)
+                
+                event = makeClassEvent(type, when, where)
+                events.append(event)
             
-            
+            newUnit = Unit(unitName[i], events)
+            self.units.append(newUnit)
+            i = i + 1
+def createCalenderEvents(units):
+        
 if __name__ == '__main__':
     
     username = input("Student ID: ")
     password = getpass.getpass()
     
-    Scraper(username, password)
+    scraper = Scraper(username, password)
+    

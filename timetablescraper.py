df572872669906f9912eb6f965402945c4f85ba4
Sam Barker <sambo.barker64@gmail.com>
2016-05-28 13:36:30 +0800

    Reformatted to more of a OO

--- timetablescraper.py
+++ timetablescraper.py
@@ -40,6 +40,7 @@ Days = {
     'Friday':4
 }
 
+TIMETABLENAME = 'University Timetable'
 class Unit:
     def __init__(self, name, classEvents):
         self.name = name
@@ -76,34 +77,6 @@ def makeClassEvent(typeE, when, where, week):
     end = endweek + day
 
     return ClassEvent(typeE, start, end, day, where)
-
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'curtin_calendar_create.json')
-
-    store = oauth2client.file.Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
     
 class Scraper:
     def __init__(self, username, password, week):
@@ -147,66 +120,117 @@ class Scraper:
             newUnit = Unit(unitName[i], events)
             self.units.append(newUnit)
             i = i + 1
+    
+  
+class GoogleCalender:
+    def __init__(self):
+        self.creds = self.get_credentials()
+        self.http = self.creds.authorize(httplib2.Http())
+        self.service = discovery.build('calendar', 'v3', http=self.http)
 
-def createEvent(classevent, unitname, calID, service):
-    event = {
-        'summary' : unitname + ' - ' +classevent.type,
-        'location' : classevent.where,
-        'start' : {
-            'dateTime' : classevent.start.strftime("%Y-%m-%dT%H:%M:00+08:00"),
-            'timeZone' : 'Australia/Perth'
-        },
-        'end' : {
-            'dateTime' : classevent.end.strftime("%Y-%m-%dT%H:%M:00+08:00"),
-            'timeZone' : 'Australia/Perth'
-        },
-            'recurrence' : [
-                'RRULE:FREQ=WEEKLY;COUNT=12'
-        ],
-        'reminders': {
-            'useDefault': False,
-            'overrides': [
-                {'method' : 'popup', 'minutes': 10}
-            ]
+        if not self.calendar_exist():
+            self.cal_id = self.createCalendar()
+
+    def get_credentials(self):
+        """Gets valid user credentials from storage.
+
+        If nothing has been stored, or if the stored credentials are invalid,
+        the OAuth2 flow is completed to obtain the new credentials.
+
+        Returns:
+            Credentials, the obtained credential. 
+        """
+        home_dir = os.path.expanduser('~')
+        credential_dir = os.path.join(home_dir, '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir,
+                                       'curtin_calendar_create.json')
+
+        store = oauth2client.file.Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+            flow.user_agent = APPLICATION_NAME
+            if flags:
+                credentials = tools.run_flow(flow, store, flags)
+            else: # Needed only for compatibility with Python 2.6
+                credentials = tools.run(flow, store)
+            print('Storing credentials to ' + credential_path)
+        return credentials
+
+    def calendar_exist(self):
+        page_token = None
+        exists = False
+
+        while True:
+          calendar_list = self.service.calendarList().list(pageToken=page_token).execute()
+          for calendar_list_entry in calendar_list['items']:
+            if calendar_list_entry['summary'] == TIMETABLENAME:
+                exists = True
+                self.cal_id = calendar_list_entry['id']
+                break
+          page_token = calendar_list.get('nextPageToken')
+          if not page_token:
+            break
+
+        return exists
+
+    def createCalendar(self):
+        calendar = {
+        'summary': 'Curtin Class Timetable',
+        'timeZone': 'Australia/Perth'
         }
-    }
 
-    event = service.events().insert(calendarId=calID, body=event).execute()
+        created_calendar = self.service.calendars().insert(body=calendar).execute()
+        print("Calender made")
+        return created_calendar['id']
+
+    def addToCalendar(self, units):
+    
+        for unit in units:
+            print("\n\nAdding events for " + unit.name)
             
-def createCalendar(creds, service):
-    calendar = {
-    'summary': 'Curtin Class Timetable',
-    'timeZone': 'Australia/Perth'
-    }
+            for event in unit.classEvents:
+                print("Adding " + event.type)
+                self.createEvent(event, unit.name)
 
-    created_calendar = service.calendars().insert(body=calendar).execute()
-    print("Calender made")
-    return created_calendar['id']
-    
-def addToCalendar(cal_id, units, service):
-    
-    for unit in units:
-        print("\n\nAdding events for " + unit.name)
-        
-        for event in unit.classEvents:
-            print("Adding " + event.type)
-            createEvent(event, unit.name, cal_id, service)  
-        
+    def createEvent(self, classevent, unitname):
+        event = {
+            'summary' : unitname + ' - ' +classevent.type,
+            'location' : classevent.where,
+            'start' : {
+                'dateTime' : classevent.start.strftime("%Y-%m-%dT%H:%M:00+08:00"),
+                'timeZone' : 'Australia/Perth'
+            },
+            'end' : {
+                'dateTime' : classevent.end.strftime("%Y-%m-%dT%H:%M:00+08:00"),
+                'timeZone' : 'Australia/Perth'
+            },
+                'recurrence' : [
+                    'RRULE:FREQ=WEEKLY;COUNT=12'
+            ],
+            'reminders': {
+                'useDefault': False,
+                'overrides': [
+                    {'method' : 'popup', 'minutes': 10}
+                ]
+            }
+        }
+
+        event = self.service.events().insert(calendarId=self.cal_id, body=event).execute()
+
 if __name__ == '__main__':
     
     username = input("Student ID: ")
-    password = getpass.getpass()
+    #password = getpass.getpass()
     day = int(input("Day: "))
     month = int(input("Month: "))
     year = int(input("Year: "))
     week = [day, month, year]
     
-    scraper = Scraper(username, password, week)
-    
-    creds = get_credentials()
-    http = creds.authorize(httplib2.Http())
-    service = discovery.build('calendar', 'v3', http=http)
-    
-    calID = createCalendar(creds, service)
-    addToCalendar(calID, scraper.units, service)
+    scraper = Scraper(username, 'Galaexy64523', week)
+
+    calendar = GoogleCalender()
+    calendar.addToCalendar(scraper.units)
     
